da8b087487a8ff0115d85abe0b89ad52d0839a16
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-05-04 16:48:47 +0800

    initializing the app

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component {
+	render() {
+		return (
+			<div className='navbar bg-light'>
+				<h4>Navbar</h4>
+			</div>
+		);
+	}
+}
+
+export default Navbar;
